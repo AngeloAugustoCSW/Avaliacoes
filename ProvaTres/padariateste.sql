7a9d7f872413737db2d5b8df75b0fb27429669f8
Angelo Augusto <71052653+AngeloAugustoCSW@users.noreply.github.com>
2021-11-30 22:07:02 -0300

    Add files via upload

--- ProvaTres/padariateste.sql
+++ ProvaTres/padariateste.sql
@@ -12,33 +12,142 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- Schema padariateste
 -- -----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS `padariateste` DEFAULT CHARACTER SET utf8 ;
+-- -----------------------------------------------------
+-- Schema padariateste
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema padariateste
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `padariateste` DEFAULT CHARACTER SET utf8 ;
 USE `padariateste` ;
 
 -- -----------------------------------------------------
--- Table `padariateste`.`Cliente`
+-- Table `padariateste`.`chef`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `padariateste`.`Cliente` (
-  `id` VARCHAR(150) NOT NULL,
-  `nome` VARCHAR(150) NULL,
-  `CPF` VARCHAR(14) NULL,
-  `dtNasc` VARCHAR(10) NULL,
-  `telefone` VARCHAR(15) NULL,
+CREATE TABLE IF NOT EXISTS `padariateste`.`chef` (
+  `id` VARCHAR(45) NOT NULL,
+  `nome` VARCHAR(45) NULL DEFAULT NULL,
+  `cpf` VARCHAR(11) NULL DEFAULT NULL,
+  `dtNasc` VARCHAR(8) NULL DEFAULT NULL,
+  `specialty` VARCHAR(25) NULL DEFAULT NULL,
+  `salario` DOUBLE NULL DEFAULT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `padariateste`.`Receita`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `padariateste`.`Receita` (
+  `id` VARCHAR(45) NOT NULL,
+  `nome` VARCHAR(45) NULL,
+  `etapas` VARCHAR(45) NULL,
+  `numEtapas` VARCHAR(45) NULL,
+  `valor` DOUBLE NULL,
+  `idChef` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_Receita_idChefx` (`idChef` ASC) ,
+  CONSTRAINT `fk_Receita_chef`
+    FOREIGN KEY (`idChef`)
+    REFERENCES `padariateste`.`chef` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `padariateste`.`Padaria`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `padariateste`.`Padaria` (
+  `id` VARCHAR(45) NOT NULL,
+  `nome` VARCHAR(45) NULL,
+  `dtAbertura` VARCHAR(45) NULL,
+  `cep` VARCHAR(9) NULL,
+  `rua` VARCHAR(45) NULL,
+  `numero` VARCHAR(45) NULL,
+  `bairro` VARCHAR(45) NULL,
+  `cidade` VARCHAR(45) NULL,
+  `horario` VARCHAR(45) NULL,
   PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `padariateste`.`Chef`
+-- Table `padariateste`.`ReceitaPadaria`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `padariateste`.`Chef` (
-  `id` VARCHAR(150) NOT NULL,
-  `nome` VARCHAR(150) NULL,
-  `CPF` VARCHAR(14) NULL,
-  `dtNasc` VARCHAR(45) NULL,
-  `specialty` VARCHAR(45) NULL,
-  `salario` VARCHAR(45) NULL,
+CREATE TABLE IF NOT EXISTS `padariateste`.`ReceitaPadaria` (
+  `idReceita` VARCHAR(45) NOT NULL,
+  `idPadaria` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`idReceita`, `idPadaria`),
+  INDEX `fk_Receita_has_Padaria_Padaria1_idx` (`idPadaria` ASC) ,
+  INDEX `fk_Receita_has_Padaria_Receita1_idx` (`idReceita` ASC) ,
+  CONSTRAINT `fk_Receita_has_Padaria_Receita1`
+    FOREIGN KEY (`idReceita`)
+    REFERENCES `padariateste`.`Receita` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Receita_has_Padaria_Padaria1`
+    FOREIGN KEY (`idPadaria`)
+    REFERENCES `padariateste`.`Padaria` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `padariateste`.`Mercado`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `padariateste`.`Mercado` (
+  `id` VARCHAR(45) NOT NULL,
+  `nome` VARCHAR(45) NULL,
+  `dtAbertura` VARCHAR(45) NULL,
+  `cep` VARCHAR(9) NULL,
+  `rua` VARCHAR(45) NULL,
+  `numero` VARCHAR(45) NULL,
+  `bairro` VARCHAR(45) NULL,
+  `cidade` VARCHAR(45) NULL,
+  `promo` VARCHAR(45) NULL,
   PRIMARY KEY (`id`))
 ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `padariateste`.`ReceitaMercado`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `padariateste`.`ReceitaMercado` (
+  `idReceita` VARCHAR(45) NOT NULL,
+  `idMercado` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`idReceita`, `idMercado`),
+  INDEX `fk_Receita_has_Mercado_Mercado1_idx` (`idMercado` ASC) ,
+  INDEX `fk_Receita_has_Mercado_Receita1_idx` (`idReceita` ASC) ,
+  CONSTRAINT `fk_Receita_has_Mercado_Receita1`
+    FOREIGN KEY (`idReceita`)
+    REFERENCES `padariateste`.`Receita` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Receita_has_Mercado_Mercado1`
+    FOREIGN KEY (`idMercado`)
+    REFERENCES `padariateste`.`Mercado` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+USE `padariateste` ;
+
+-- -----------------------------------------------------
+-- Table `padariateste`.`cliente`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `padariateste`.`cliente` (
+  `id` VARCHAR(45) NOT NULL,
+  `nome` VARCHAR(45) NULL DEFAULT NULL,
+  `cpf` VARCHAR(11) NULL DEFAULT NULL,
+  `dtNasc` VARCHAR(8) NULL DEFAULT NULL,
+  `telefone` VARCHAR(11) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
